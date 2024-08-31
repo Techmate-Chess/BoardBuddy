c939ef549fc2fd0dde6c1b7a06aa4937cc514e3c
SaggingRufus <saggingrufus@outlook.com>
2024-08-31 08:56:29 -0300

    attempt at a working base project

---
+++ core/src/test/java/com/rufus/techmate/boardbuddy/TestingThing.java
@@ -0,0 +1,11 @@
+package com.rufus.techmate.boardbuddy;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestingThing {
+  @Test
+  public void testTheDarnFramework() {
+    Assert.assertEquals(1, 1);
+  }
+}
--- launcher/src/main/java/com/rufus/techmate/launcher/Run.java
+++ launcher/src/main/java/com/rufus/techmate/launcher/Run.java
@@ -1,14 +1,16 @@
 package com.rufus.techmate.launcher;
 
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
 public class Run {
-    public static void main(String[] args) {
-TomcatLoadTimeWeaver tomcat = new Tomcat();
+  public static void main(String[] args) throws LifecycleException {
+    Tomcat tomcat = new Tomcat();
+    tomcat.setPort(8080);
 
-        tomcat.setPort(8080);
+    tomcat.addWebapp("/", new java.io.File("src/main/webapp").getAbsolutePath());
 
-        tomcat.addWebapp("/", new java.io.File("src/main/webapp").getAbsolutePath());
-
-        tomcat.start();
-        tomcat.getServer().await();
-    }
+    tomcat.start();
+    tomcat.getServer().await();
+  }
 }
